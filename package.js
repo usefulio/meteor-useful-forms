cb6bce5079a9987c835b22814010ad22792d86a8
Joshua Ohlman <codewriterohs@gmail.com>
2014-10-07 09:24:13 -0500

    Add package metadata for 0.1.0 release

--- package.js
+++ package.js
@@ -1,18 +1,22 @@
 Package.describe({
-  summary: "REPLACEME - What does this package (or the original one you're wrapping) do?"
+  summary: "A minimalistic forms library with reactive updates and validation."
+  , name: "cwohlman:forms"
+  , git: "https://github.com/cwohlman/meteor-useful-forms.git"
+  , version: "0.1.0"
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom('0.9.3');
   api.use('templating');
   api.use('ui');
-  api.use('cwohlman:shadow-objects@0.1.0');
+  api.use('cwohlman:shadow-objects@0.1.1');
   api.use('reactive-var');
   api.add_files('forms.html', ['client']);
   api.add_files('forms.js', ['client']);
 });
 
 Package.on_test(function (api) {
-  api.use('forms');
+  api.use('cwohlman:forms');
 
   api.add_files('forms_tests.js', ['client', 'server']);
 });
