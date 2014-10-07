1af81c52fc702ce3669939266f22457d1c835d83
Joshua Ohlman <codewriterohs@gmail.com>
2014-10-07 16:44:33 -0500

    Bump version

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "A minimalistic forms library with reactive updates and validation."
   , name: "cwohlman:forms"
   , git: "https://github.com/cwohlman/meteor-useful-forms.git"
-  , version: "0.2.0"
+  , version: "0.2.1"
 });
 
 Package.on_use(function (api, where) {
